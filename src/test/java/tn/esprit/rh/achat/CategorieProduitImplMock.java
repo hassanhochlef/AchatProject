7bd2f2181aa073cfcaaaaf46b169a0e22b6b56af
hassanhochlef <73234987+hassanhochlef@users.noreply.github.com>
2022-11-08 12:21:47 +0100

    test

--- src/test/java/tn/esprit/rh/achat/CategorieProduitImplMock.java
+++ src/test/java/tn/esprit/rh/achat/CategorieProduitImplMock.java
@@ -1,11 +1,14 @@
 package tn.esprit.rh.achat;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.verify;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import tn.esprit.rh.achat.controllers.converter.CategorieProduitConverter;
 import tn.esprit.rh.achat.controllers.dto.CategorieProduitDTO;
+import tn.esprit.rh.achat.controllers.dto.ProduitDTO;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
@@ -36,29 +41,45 @@ import tn.esprit.rh.achat.services.CategorieProduitServiceImpl;
 @ExtendWith(MockitoExtension.class)
 class CategorieProduitImplMock {
 @Mock
-CategorieProduitRepository categorieProduitRepository;
+CategorieProduitRepository categorieProduitRepository2;
 @InjectMocks
 CategorieProduitServiceImpl ctagorieProduitService;
 @Autowired
 CategorieProduitConverter categorieProduitConverter;
-@Autowired
-ModelMapper modelMapper;
+@InjectMocks
+CategorieProduitConverter categorieProduitConverter1;
+@Autowired 
+CategorieProduitRepository categorieProduitRepository;
+CategorieProduit cp1 = new CategorieProduit((long) 2,"codeCategorie","libelle",null);
 
+Long getId() {
+	for(CategorieProduit cp : categorieProduitRepository.findAll()) {
+		return cp.getIdCategorieProduit();
+	}
+	return 0L;
+}
+@Test
+void Testdeleteecp() {
+	
+	 categorieProduitRepository2.deleteById((long) 2);
+	verify(categorieProduitRepository2,atLeastOnce()).deleteById((long) 2);
+}
 @Test
 void TestAddCategorieProduit() {
 	Set<Produit> prods = new HashSet<Produit>();
 	CategorieProduit cp = new CategorieProduit((long) 2,"codeCategorie","libelle",prods);
-	CategorieProduit c = new CategorieProduit((long)1,"a","b", null);
-	lenient().when(categorieProduitRepository.save(Mockito.any(CategorieProduit.class)))
-	.thenReturn(c);
+	
+	categorieProduitRepository2.save(cp);
+	verify(categorieProduitRepository2,atLeastOnce()).save(cp);
 	Assertions.assertNotNull(cp);
+	System.out.println(cp);
 }
 @Test
-void TestAddCategorieProduit1() {
-	Set<Produit> prods = new HashSet<Produit>();
-	CategorieProduit cp = new CategorieProduit((long) 2,"codeCategorie","libelle",prods);
-	CategorieProduitDTO cpD = categorieProduitConverter.convertEntityToDto(cp);
-ctagorieProduitService.addorUpdateCategorieProduit(cpD);
-Assertions.assertNotNull(cp);
+
+void TestRetrieveCategorieProduit() {
+	Optional<CategorieProduit> cp = categorieProduitRepository2.findById((long)2);
+	verify(categorieProduitRepository2,atLeastOnce()).findById((long) 2);
+	Assertions.assertNotNull(cp);
 }
+
 }
