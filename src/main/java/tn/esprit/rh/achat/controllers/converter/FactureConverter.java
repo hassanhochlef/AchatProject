5ba8a7dc8b92ccf16efdd766398ea55b55dee0a6
achref benhadjyahia <achref.benhadjyahia@esprit.tn>
2022-11-09 17:41:25 +0100

    sonarquality

--- src/main/java/tn/esprit/rh/achat/controllers/converter/FactureConverter.java
+++ src/main/java/tn/esprit/rh/achat/controllers/converter/FactureConverter.java
@@ -5,7 +5,7 @@ import org.modelmapper.ModelMapper;
 import tn.esprit.rh.achat.controllers.dto.FactureDTO;
 import tn.esprit.rh.achat.entities.Facture;
 
-public class factureConverter {
+public class FactureConverter {
     public FactureDTO convertEntityToDto(Facture facture) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(facture, FactureDTO.class);
--- src/main/java/tn/esprit/rh/achat/controllers/dto/FactureDTO.java
+++ src/main/java/tn/esprit/rh/achat/controllers/dto/FactureDTO.java
@@ -1,15 +1,11 @@
 package tn.esprit.rh.achat.controllers.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import tn.esprit.rh.achat.entities.DetailFacture;
-import tn.esprit.rh.achat.entities.Fournisseur;
-import tn.esprit.rh.achat.entities.Reglement;
 
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
-import java.util.Set;
+
 @Data
 public class FactureDTO {
     private Long idFacture;
