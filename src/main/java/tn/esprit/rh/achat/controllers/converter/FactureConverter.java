05d29a420a4f658ac019fd035869c474058a7c57
achref benhadjyahia <achref.benhadjyahia@esprit.tn>
2022-11-10 02:07:06 +0100

    up

--- src/main/java/tn/esprit/rh/achat/controllers/converter/FactureConverter.java
+++ src/main/java/tn/esprit/rh/achat/controllers/converter/FactureConverter.java
@@ -2,9 +2,10 @@ package tn.esprit.rh.achat.controllers.converter;
 
 
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 import tn.esprit.rh.achat.controllers.dto.FactureDTO;
 import tn.esprit.rh.achat.entities.Facture;
-
+@Component
 public class FactureConverter {
     public FactureDTO convertEntityToDto(Facture facture) {
         ModelMapper modelMapper = new ModelMapper();
